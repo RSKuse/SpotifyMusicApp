676c28b494d5fbe7fadd555c715b60903bff4341
Reuben Simphiwe Kuse <sim.khuse@gmail.com>
2024-06-19 23:03:01 +0200

    added static var for images

---
+++ SpotifyMusicApp/SpotifyImages.swift
@@ -0,0 +1,28 @@
+//
+//  SpotifyImages.swift
+//  SpotifyMusicApp
+//
+//  Created by Reuben Simphiwe Kuse on 2024/06/19.
+//
+
+import Foundation
+import UIKit
+
+class SpotifyImages {
+    
+    static var albumCoverImage = UIImage(named: "album_cover")
+    static var shuffleButton = UIImage(named: "shuffle_icon")
+    static var playPauseButton = UIImage(named: "play_icon")
+    static var previousButton = UIImage(named: "previous_icon")
+    static var nextButton = UIImage(named: "next_icon")
+    static var addButton = UIImage(named: "add_icon")
+    static var timerButton = UIImage(named: "timer_icon")
+    static var shareButton = UIImage(named: "share_icon")
+    static var deviceButton = UIImage(named: "device_icon")
+    static var downArrowButton = UIImage(named: "down_arrow_icon")
+    static var moreOptionsButton = UIImage(named: "more_options_icon")
+
+    
+}
+
+
--- SpotifyMusicApp/ViewController.swift
+++ SpotifyMusicApp/ViewController.swift
@@ -10,9 +10,9 @@ import UIKit
 class ViewController: UIViewController {
     var isPlaying = false
     
-        lazy var albumCoverImageView: UIImageView = {
+    lazy var albumCoverImageView: UIImageView = {
         let imageView = UIImageView()
-        imageView.image = UIImage(named: "album_cover")
+        imageView.image = SpotifyImages.albumCoverImage
         imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
         imageView.layer.cornerRadius = 5
@@ -54,32 +54,35 @@ class ViewController: UIViewController {
     
     lazy var shuffleButton: UIButton = {
         let button = UIButton(type: .system)
-        button.setImage(UIImage(named: "shuffle_icon"), for: .normal)
+        button.setImage(SpotifyImages.shuffleButton, for: .normal)
         button.tintColor = .white
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-    
-    lazy var previousButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setImage(UIImage(named: "previous_icon"), for: .normal)
-        button.tintColor = .white
+        button.addTarget(self, action: #selector(shuffleButtonTapped), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
     
     lazy var playPauseButton: UIButton = {
         let button = UIButton(type: .system)
-        button.setImage(UIImage(named: "play_icon"), for: .normal)
+        button.setImage(SpotifyImages.playPauseButton, for: .normal)
         button.tintColor = .white
         button.addTarget(self, action: #selector(playPauseButtonTapped), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
     
+    lazy var previousButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(SpotifyImages.previousButton, for: .normal)
+        button.tintColor = .white
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    
+    
     lazy var nextButton: UIButton = {
         let button = UIButton(type: .system)
-        button.setImage(UIImage(named: "next_icon"), for: .normal)
+        button.setImage(SpotifyImages.nextButton, for: .normal)
         button.tintColor = .white
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
@@ -94,10 +97,10 @@ class ViewController: UIViewController {
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
-
+    
     lazy var addButton: UIButton = {
         let button = UIButton(type: .system)
-        button.setImage(UIImage(named: "add_icon"), for: .normal)
+        button.setImage(SpotifyImages.addButton, for: .normal)
         button.tintColor = .white
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
@@ -105,7 +108,7 @@ class ViewController: UIViewController {
     
     lazy var timerButton: UIButton = {
         let button = UIButton(type: .system)
-        button.setImage(UIImage(named: "timer_icon"), for: .normal)
+        button.setImage(SpotifyImages.timerButton, for: .normal)
         button.tintColor = .white
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
@@ -113,7 +116,7 @@ class ViewController: UIViewController {
     
     lazy var shareButton: UIButton = {
         let button = UIButton(type: .system)
-        button.setImage(UIImage(named: "share_icon"), for: .normal)
+        button.setImage(SpotifyImages.shareButton, for: .normal)
         button.tintColor = .white
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
@@ -148,7 +151,7 @@ class ViewController: UIViewController {
     
     lazy var deviceButton: UIButton = {
         let button = UIButton(type: .system)
-        button.setImage(UIImage(named: "device_icon"), for: .normal)
+        button.setImage(SpotifyImages.deviceButton, for: .normal)
         button.tintColor = .white
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
@@ -165,7 +168,7 @@ class ViewController: UIViewController {
     
     lazy var downArrowButton: UIButton = {
         let button = UIButton(type: .system)
-        button.setImage(UIImage(named: "down_arrow_icon"), for: .normal)
+        button.setImage(SpotifyImages.downArrowButton, for: .normal)
         button.tintColor = .white
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
@@ -173,7 +176,7 @@ class ViewController: UIViewController {
     
     lazy var moreOptionsButton: UIButton = {
         let button = UIButton(type: .system)
-        button.setImage(UIImage(named: "more_options_icon"), for: .normal)
+        button.setImage(SpotifyImages.moreOptionsButton, for: .normal)
         button.tintColor = .white
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
@@ -224,7 +227,7 @@ class ViewController: UIViewController {
         
         songAndArtistTitleStackView.topAnchor.constraint(equalTo: albumCoverImageView.bottomAnchor, constant: 40).isActive = true
         songAndArtistTitleStackView.leftAnchor.constraint(equalTo: durationSlider.leftAnchor).isActive = true
-            
+        
         addButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
         addButton.centerYAnchor.constraint(equalTo: songAndArtistTitleStackView.centerYAnchor).isActive = true
         addButton.widthAnchor.constraint(equalToConstant: 24).isActive = true
@@ -253,10 +256,10 @@ class ViewController: UIViewController {
         
         previousButton.widthAnchor.constraint(equalToConstant: 25).isActive = true
         previousButton.heightAnchor.constraint(equalToConstant: 25).isActive = true
-
+        
         playPauseButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
         playPauseButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
-
+        
         nextButton.widthAnchor.constraint(equalToConstant: 25).isActive = true
         nextButton.heightAnchor.constraint(equalToConstant: 25).isActive = true
         
@@ -264,17 +267,22 @@ class ViewController: UIViewController {
         deviceButton.topAnchor.constraint(equalTo: shufflePlayRewindStackView.bottomAnchor, constant: 20).isActive = true
         deviceButton.widthAnchor.constraint(equalToConstant: 25).isActive = true
         deviceButton.heightAnchor.constraint(equalToConstant: 25).isActive = true
-            
+        
         shareButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
         shareButton.topAnchor.constraint(equalTo: timerButton.bottomAnchor, constant: 20).isActive = true
         shareButton.widthAnchor.constraint(equalToConstant: 45).isActive = true
         shareButton.heightAnchor.constraint(equalToConstant: 45).isActive = true
-            
+        
     }
     
     @objc func playPauseButtonTapped() {
         isPlaying.toggle()
         let buttonImage = isPlaying ? UIImage(named: "pause_icon") : UIImage(named: "play_icon")
         playPauseButton.setImage(buttonImage, for: .normal)
+    }
+    
+    @objc func shuffleButtonTapped() {
+        let spotifyGreen = UIColor(red: 30/255, green: 215/255, blue: 96/255, alpha: 1)
+        shuffleButton.tintColor = shuffleButton.tintColor == .white ? spotifyGreen : .white
     }
 }
